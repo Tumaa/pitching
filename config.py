3caf740944cb5d0347631088e854861a18b5ae0e
tumaa <ramanfatuu@gmail.com>
2019-10-23 10:49:51 +0300

    prepare app for deployment

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
 
     '''
-    # SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fatuma:qwerty12@localhost/tuma'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fatuma:qwerty12@localhost/tuma'
     DEBUG = True
 
 config_options = {
