2ae2a8ba3c425658ac9cb986ab74898365398d2b
tumaa <ramanfatuu@gmail.com>
2019-10-25 01:37:12 +0300

    changes

--- manage.py
+++ manage.py
@@ -2,9 +2,12 @@ from app import create_app,db
 from flask_script import Manager, Server
 from app.models import User,Pitch,Comment
 from flask_migrate import Migrate, MigrateCommand
+
+
 app =  create_app('production')
 manager = Manager(app)
 manager.add_command('server', Server)
+
 @manager.command
 def test():
    '''run unittest'''
