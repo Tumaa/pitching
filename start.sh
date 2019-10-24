314b2315af6931447fb150dee572e44660d061a0
tumaa <ramanfatuu@gmail.com>
2019-10-25 02:23:08 +0300

    chanhes in gitignore

---
+++ start.sh
@@ -0,0 +1,5 @@
+export MAIL_USERNAME="developersjuniors@gmail.com"
+export MAIL_PASSWORD="Nairobi001"
+export SECRET_KEY="123456"
+export DATABASE_URI="postgresql+psycopg2://fatuma:qwerty12@localhost/tuma00"
+python3.6 manage.py server
