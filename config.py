9e4994cd1fcb36c16dea95e145f80582297e7dca
fatma-kadubo <ramanfatuu@gmail.com>
2019-10-24 16:50:54 +0300

    changed status

--- config.py
+++ config.py
@@ -27,7 +27,7 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    pass
+    # SQLALCHEMY_DATABASE_URI = 'ppostgresql+psycopg2://fatma:qwerty123456@localhost/fatumaa'
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class TestConfig(Config):
