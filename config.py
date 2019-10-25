cd0dd56f3f410b0fabb3d123089bf79f71ff217c
tumaa <ramanfatuu@gmail.com>
2019-10-25 08:15:11 +0300

    changes in the alembic file

--- config.py
+++ config.py
@@ -11,11 +11,11 @@ class Config:
     
 
     #  email configurations
-    # MAIL_SERVER = 'smtp.googlemail.com'
-    # MAIL_PORT = 587
-    # MAIL_USE_TLS = True
-    # MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    # MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     
     DEBUG = True
 
