ede2451f1060c4d6375efbba801bbcdb4d1a266f
tumaa <ramanfatuu@gmail.com>
2019-10-25 12:52:45 +0300

    changed development mode

--- config.py
+++ config.py
@@ -1,33 +1,41 @@
 import os
-
 class Config:
-    """
-    This is the parent class which will have the general configurations
-    """
-    UPLOADED_PHOTOS_DEST = "app/static/photos"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-
-    #email configurations
-    MAIL_SERVER = 'smtp.googlemail.com'
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-
+   '''
+   General configuration parent class
+   '''
+   pass
+   # simple mde  configurations
+   SIMPLEMDE_JS_IIFE = True
+   SIMPLEMDE_USE_CDN = True
+   UPLOADED_PHOTOS_DEST ='app/static/photos'
+   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fatuma:123456kenya@localhost/yumna00'
+   #  email configurations
+   MAIL_SERVER = 'smtp.googlemail.com'
+   MAIL_PORT = 587
+   MAIL_USE_TLS = True
+   MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+   MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 class ProdConfig(Config):
-    SECRET_KEY = os.environ.get("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI =os.environ.get('DATABASE_URL')
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://fatuma:123456kenya@localhost/yumna00'
-    DEBUG = True
-
+   '''
+   Production  configuration child class
+   Args:
+       Config: The parent configuration class with General configuration settings
+   '''
+   pass
+   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+class TestConfig(Config):
+   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fatuma:123456kenya@localhost/yumna00'
 class DevConfig(Config):
-    SECRET_KEY='12345'
-    SQLALCHEMY_DATABASE_URI ='postgresql+psycopg2://fatuma:123456kenya@localhost/yumna00'
-    DEBUG = True
-    pass
-
+   '''
+   Development  configuration child class
+   Args:
+       Config: The parent configuration class with General configuration settings
+   '''
+   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://fatuma:123456kenya@localhost/yumna00'
+   DEBUG = True
 config_options = {
 'development':DevConfig,
-'production':ProdConfig
+'production':ProdConfig,
+'test':TestConfig
 }
--- manage.py
+++ manage.py
@@ -1,30 +1,28 @@
-from app import create_app,db
-from flask_script import Manager,Server
+
+from app import create_app, db
+from flask_script import Manager, Server
 from app.models import User,Pitch,Comment
 from flask_migrate import Migrate, MigrateCommand
-  
-  #creating app instance
-app = create_app('production')
 
+
+app =  create_app('development')
 manager = Manager(app)
-manager.add_command('server',Server)
+manager.add_command('server', Server)
 
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
 
 @manager.command
 def test():
-    '''
-    Run unit test
-    '''
-    import unittest
-    tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
-
-
+   '''run unittest'''
+   import unittest
+   tests = unittest.TestLoader().discover('test')
+   unittest.TextTestRunner(verbosity=2).run(tests)
+   
+   
 @manager.shell
 def make_shell_context():
     return dict(app = app,db = db,User = User )
-
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
 if __name__ == '__main__':
-    manager.run()
+   app.secret_key = '123456'
+   manager.run()
