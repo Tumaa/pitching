44ee1d8728a9ab748c94652ea8a2e4d66d8b22e5
tumaa <ramanfatuu@gmail.com>
2019-10-25 13:42:29 +0300

    update db migrations

--- pitching/config.py
+++ pitching/config.py
@@ -6,3 +6,29 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME='developersjunior0@gmail.com'
     MAIL_PASSWORD='Nairobi001'
+    
+    
+class ProdConfig(Config):
+   '''
+   Pruduction configuration child class
+   Args:
+       Config: The parent configuration class with General configuration settings
+   '''
+   SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:qwerty12@localhost/tuma00'
+class TestConfig(Config):
+  '''
+  Testing configuration child class
+  Args:
+      Config: The parent configuration class with General configuration settings
+  '''
+  SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:qwerty12@localhost/tuma00'
+  pass
+class DevConfig(Config):
+   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:qwerty12@localhost/tuma00'
+   DEBUG = True
+config_options ={
+   'development':DevConfig,
+   'production':ProdConfig,
+   'test': TestConfig
+   }
